5f58f252b21237fef1f0fad4413cd402f3569ada
Mason Choi <choi.mason@gmail.com>
2023-01-11 22:19:02 -0500

    gmaps data collection research

--- data_collection.py
+++ data_collection.py
@@ -1,9 +1,19 @@
 # Aggregates images from around world with corresponding location information
 # including continent, country, and (state?) city
-import imports
+# Place id to location: https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJrTLr-GyuEmsRBfy61i59si0&key=APIKEY
+# Coords to road coors: https://roads.googleapis.com/v1/snapToRoads?interpolate=true&path=-35.284728747199374%2C149.12834860726772%7C-35.28032%2C149.12907%7C-35.28099%2C149.12929%7C-35.28144%2C149.12984%7C-35.28194%2C149.13003%7C-35.28282%2C149.12956%7C-35.28302%2C149.12881%7C-35.28473%2C149.12836&key=APIKEY
+# Coords to image: https://maps.googleapis.com/maps/api/streetview?size=600x300&location=-35.2784195,149.12946589999999&heading=120&key=APIKEY
+# Download online image: https://stackoverflow.com/questions/30229231/python-save-image-from-url
+
+import requests
 
 
 def get_data(type):
+    image_url = "test url"
+    img_data = requests.get(image_url).content
+    with open('image_name.jpg', 'wb') as handler:
+        handler.write(img_data)
+
     # type is the level of geographical specificity: continent, country, or state/city
     images = []  # RGB pixel matrix
     labels = []  # Label strings to be converted to scalars
