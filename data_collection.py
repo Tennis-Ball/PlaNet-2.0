29cd8450337a3d9e1fbcd20fb863884fa84cdec8
Mason Choi <choi.mason@gmail.com>
2023-01-12 21:23:56 -0500

    API approach

--- data_collection.py
+++ data_collection.py
@@ -6,6 +6,14 @@
 # Download online image: https://stackoverflow.com/questions/30229231/python-save-image-from-url
 
 import requests
+import re
+
+
+def extract_coords(json_string):
+    segment = json_string[json_string.find("latitude")+11:]
+    latitude = segment[:segment.find(",")]
+    longitude = segment[segment.find("longitude")+12:segment.find("\n      }")]
+    return latitude, longitude
 
 
 def get_data(type):
@@ -19,3 +27,24 @@ def get_data(type):
     labels = []  # Label strings to be converted to scalars
     return images, labels
 
+lat, long = "-35.284728747199374", "149.12834860726772"
+lat, long = "42.3436709", "-71.0768575"
+seen = [(lat, long)]
+json_string = requests.get(f"https://roads.googleapis.com/v1/snapToRoads?interpolate=true&path={lat}%2C{long}&key=APIKEY").text
+lat, long = extract_coords(json_string)
+
+while (lat, long) not in seen:
+    seen.append((lat, long))
+    json_string = requests.get(f"https://roads.googleapis.com/v1/snapToRoads?interpolate=true&path={lat}%2C{long}&key=APIKEY").text
+    print(json_string)
+    lat, long = extract_coords(json_string)
+    print(len(seen))
+
+print((lat, long))
+print(seen)
+
+for i in range(7**24):
+    if i % (1000000000000000) == 0:
+        print(i)
+
+print("done")
